728e944a35070e567578c2c349e868afc4279cfb
Sarah Kuss <sarah.kuss33@gmail.com>
2023-04-04 16:09:41 -0400

    added endpoint and imported tvLibrary

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 import express from 'express'
 import cors from 'cors'
 
+// import CRUD functions
+import { addNewTV, getAllTv } from './src/tvLibrary.js'
+
 const PORT = 3000
 const app = express()
 
 app.use(cors())
 app.use(express.json())
+
+// get all tv shows
+app.get('/tvShows', getAllTv )
+// add tvshows
+app.post('/tvShows', addNewTV)
 
 app.listen(PORT, () => {
   console.log(`Listening on http://localhost:${PORT}`)
