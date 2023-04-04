4c093b04e81048bacc163a86ef6ecf9d294cf64c
Sarah Kuss <sarah.kuss33@gmail.com>
2023-04-04 17:00:21 -0400

    fixed errors

--- index.js
+++ index.js
@@ -11,9 +11,9 @@ app.use(cors())
 app.use(express.json())
 
 // get all tv shows
-app.get('/tvShows', getAllTv )
+app.get('/tvshows', getAllTv )
 // add tvshows
-app.post('/tvShows', addNewTV)
+app.post('/tvshows', addNewTV)
 
 app.listen(PORT, () => {
   console.log(`Listening on http://localhost:${PORT}`)
