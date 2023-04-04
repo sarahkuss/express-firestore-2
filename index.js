88d9daf0f30df8e90b436ab715fc81c278d50fdd
Sarah Kuss <sarah.kuss33@gmail.com>
2023-04-04 15:10:21 -0400

    first phase

---
+++ index.js
@@ -0,0 +1,12 @@
+import express from 'express'
+import cors from 'cors'
+
+const PORT = 3000
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.listen(PORT, () => {
+  console.log(`Listening on http://localhost:${PORT}`)
+})
